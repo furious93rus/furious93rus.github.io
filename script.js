d25a3ed38ef355f72ec47f0c477042947a42b448
furious93rus <56839415+furious93rus@users.noreply.github.com>
2020-10-20 17:25:33 +0300

    Hello world commit!

---
+++ script.js
@@ -0,0 +1,32 @@
+function goInteractive() {
+
+    let form = document.getElementById('phone-number');
+    let phone = form.value;
+    let submitButton = document.getElementById('submit-button');
+    let checkbox = document.getElementById('web-version');
+    let useWebVersion = false;
+
+    form.addEventListener('input', (e) => {
+        e.preventDefault();
+        !form.value.match(/\D+/g) && form.value.length <= 11 ? phone = form.value : form.value = phone;
+        submitButton.disabled = phone.length < 11;
+    });
+
+    form.addEventListener('blur', (e) => {
+        e.preventDefault();
+        form.isValid = phone.length === 11
+    });
+
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.location.href = `http://${checkbox.checked ? 'web' : 'api'}.whatsapp.com/send?phone=${phone}`
+    });
+
+    // checkbox.addEventListener('click', (e) => {
+    //     e.preventDefault();
+    //     useWebVersion =
+    // })
+
+}
+
+goInteractive();
